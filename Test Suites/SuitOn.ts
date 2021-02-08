9e1822d15628eedd568a171ce64300aa240077a5
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-02-08 22:15:01 +0530

    First commit

---
+++ Test Suites/SuitOn.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuitOn</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f7a48065-5026-4d7f-9078-4973d3749dcd</testSuiteGuid>
+   <testCaseLink>
+      <guid>2f2a3441-59b3-42f6-b82a-a357089ded28</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestOn</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
