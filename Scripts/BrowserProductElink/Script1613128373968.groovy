50092a7996105b10bdf1a48cdbd88c38e1356592
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-02-12 17:11:26 +0530

    waits added

--- Scripts/BrowserProductElink/Script1613128373968.groovy
+++ Scripts/BrowserProductElink/Script1613128373968.groovy
@@ -9,22 +9,29 @@ import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
-
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-
 import internal.GlobalVariable as GlobalVariable
+
 
 WebUI.openBrowser(GlobalVariable.RegalURL)
 
+WebUI.waitForElementClickable(findTestObject('HomePage/ProductList/Products'), 5000)
+
 WebUI.mouseOver(findTestObject('HomePage/ProductList/Products'))
 
+WebUI.waitForElementClickable(findTestObject('HomePage/ProductList/Bearings'), 5000)
+
 WebUI.click(findTestObject('HomePage/ProductList/Bearings'))
+
+WebUI.waitForElementClickable(findTestObject('HomePage/ProductList/SmartInterchage'), 5000)
 
 WebUI.click(findTestObject('HomePage/ProductList/SmartInterchage'))
 
 WebUI.switchToWindowIndex(1)
+
+WebUI.waitForElementClickable(findTestObject('HomePage/ProductList/RegalIcon'), 5000)
 
 WebUI.click(findTestObject('HomePage/ProductList/RegalIcon'))
 
