c59697f496868e8ffb91e318707f0bbbd21b9924
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-02-12 17:40:45 +0530

    test

--- Scripts/TestExample/Script1612881511166.groovy
+++ Scripts/TestExample/Script1612881511166.groovy
@@ -10,7 +10,8 @@ import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
-
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser(GlobalVariable.RegalURL)
