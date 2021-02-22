9b06409ff281a1a744e59da313c5be9b8d512933
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-02-22 12:54:57 +0530

    windows built in keyword import removed

--- Scripts/BrowserProductElink/Script1613128373968.groovy
+++ Scripts/BrowserProductElink/Script1613128373968.groovy
@@ -13,7 +13,7 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
 
 
 WebUI.openBrowser(GlobalVariable.RegalURL)
--- Scripts/HVAC_Motor_Compare/Script1613396292878.groovy
+++ Scripts/HVAC_Motor_Compare/Script1613396292878.groovy
@@ -13,7 +13,7 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
 import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser(GlobalVariable.RegalURL)
