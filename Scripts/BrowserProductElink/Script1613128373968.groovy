25fe5f3b8eccc053241290c0c8f5f5c6d90801e3
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-02-22 12:52:56 +0530

    Issue resolved

--- Scripts/BrowserProductElink/Script1613128373968.groovy
+++ Scripts/BrowserProductElink/Script1613128373968.groovy
@@ -14,7 +14,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+
 
 WebUI.openBrowser(GlobalVariable.RegalURL)
 
