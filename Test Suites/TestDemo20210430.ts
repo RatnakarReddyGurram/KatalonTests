974270dc7d9f997dd9ee22cda4c7ca9f245aae86
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-04-30 19:37:44 +0530

    New Test suit

---
+++ Test Suites/TestDemo20210430.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestDemo20210430</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f96d3de9-e448-4b8a-baea-70153eed1655</testSuiteGuid>
+   <testCaseLink>
+      <guid>96d22566-0661-408b-a412-487e1df84981</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Test Cases/Login Verification</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2b150fbf-0c61-411b-ae11-165c2f2f6dc1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Test Cases/Logout Verification</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
