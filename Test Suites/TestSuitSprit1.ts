6f377eccb2fe63e121bc98c40628fd13c88368d5
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-05-11 17:31:13 +0530

    new testsuit changes

---
+++ Test Suites/TestSuitSprit1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuitSprit1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>04beaeca-c223-406e-837f-69dfd9d1fa52</testSuiteGuid>
+   <testCaseLink>
+      <guid>7143cedd-8dc3-4d59-bcbf-efb425ce83b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Test Cases/Logout Verification</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>66942d9c-84e2-45b4-86f4-311e7dad051f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Test Cases/Login Verification</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
