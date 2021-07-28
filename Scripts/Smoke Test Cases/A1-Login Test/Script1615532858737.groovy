91facd74a2e3d060f574c2ad98ade997c6b111c8
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-07-28 20:52:42 +0530

    fixed

--- Scripts/Smoke Test Cases/A1-Login Test/Script1615532858737.groovy
+++ Scripts/Smoke Test Cases/A1-Login Test/Script1615532858737.groovy
@@ -10,8 +10,8 @@ import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-//import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+//import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 //import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
