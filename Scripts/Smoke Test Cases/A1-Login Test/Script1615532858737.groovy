b3b41aace0122f3c89bb670b9842a637e076db6b
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-07-28 20:50:58 +0530

    issue fixed

--- Scripts/Smoke Test Cases/A1-Login Test/Script1615532858737.groovy
+++ Scripts/Smoke Test Cases/A1-Login Test/Script1615532858737.groovy
@@ -11,8 +11,8 @@ import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+//import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+//import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser(GlobalVariable.RegalURL)
