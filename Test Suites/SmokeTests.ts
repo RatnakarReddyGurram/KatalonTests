394f365485044dde086a7c65b537f401738c59b0
900015183 <900015183@HYDLT-7WKMTT2.rbc.int>
2021-08-04 15:51:31 +0530

    Test Suit update

---
+++ Test Suites/SmokeTests.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SmokeTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7b957191-2bf5-4919-bb01-893a01535e47</testSuiteGuid>
+   <testCaseLink>
+      <guid>27a430ee-a266-4ca9-9579-006d57f5f8e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Test Cases/A1-Login Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3319c45b-2365-436e-bfa1-92dca51611ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Test Cases/Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
